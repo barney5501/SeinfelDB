32e45dec13074d846e0db1fc1ed22b5fada5ac6d
Itay <Itaybn100@gmail.com>
2026-07-01 00:23:18 +0300

    feat: add script for batch pinecone upsert

---
+++ pinecone_upsert.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone
+import json
+
+load_dotenv()
+PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
+INDEX_HOST = os.environ["INDEX_HOST"]
+
+with open("seinfeld.json", "r") as seinfile:
+    records = json.load(seinfile)
+
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index = pc.Index(host=INDEX_HOST)
+
+BATCH_SIZE = 64
+for i in range(13504, len(records), BATCH_SIZE):
+    batch = records[i : i + BATCH_SIZE]
+    print(f"{i}/{len(records)}")
+    print("...")
+    index.upsert_records(namespace="__default__", records=batch)
+print("done")
