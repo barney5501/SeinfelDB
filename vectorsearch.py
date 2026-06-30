e5a3df5bed90191ad89f9d4b045e367e542617e4
Itay <Itaybn100@gmail.com>
2026-07-01 02:28:01 +0300

    feat: add loading messages

--- vectorsearch.py
+++ vectorsearch.py
@@ -1,22 +1,32 @@
 from pinecone import Pinecone
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 
 pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 index = pc.Index(host=os.environ["PINECONE_INDEX_HOST"])
+messages = [
+    "🍤Looking for the perfect comback...",
+    "📦Opening the vault...",
+    "📪Asking Newman...",
+]
 
 
 def search(query):
     try:
+        message = random.choice(messages)
+        message_status = {"SEID": "/", "Character": "/", "Dialogue": message}
+        yield message_status
+
         results = index.search(
             namespace="__default__",
             query={"inputs": {"text": query}, "top_k": 5},
             fields=["SEID", "Character", "Dialogue"],
         )
         refrences = [result["fields"] for result in results["result"]["hits"]]
-        return refrences
+        yield refrences
     except Exception as e:
         print(e)
         return [
